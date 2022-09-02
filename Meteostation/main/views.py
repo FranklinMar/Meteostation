596b60108151374e8f66a279c6cc16e15581145b
denisbereznuuk <denisbereznuuk@gmail.com>
2022-09-02 20:09:47 +0300

    Data file upload view done

--- Meteostation/main/views.py
+++ Meteostation/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Data
+from django.core.files.storage import FileSystemStorage
+# from Meteostation.Meteostation.settings import MEDIA_ROOT
+import pandas as pd
+import os
 
 
 # Create your views here.
@@ -14,5 +18,15 @@ def home(request):
 
 
 def data(request):
+    if request.method == "POST":
+        file = request.FILES['upload']
+        print(file.name)
+        # print(file.size)
+        fs = FileSystemStorage()
+        fs.save(file.name, file)
+        # read_file = pd.read_excel(os.path.join(MEDIA_ROOT), file.name)
+        # print(type(read_file).__name__)
+        # print(read_file)
+
     return render(request, "main/data.html")
 
