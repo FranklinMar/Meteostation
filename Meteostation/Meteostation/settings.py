61f153d6eb12dff77e5ce5979099ae3a49efbe4b
denisbereznuuk <denisbereznuuk@gmail.com>
2022-09-09 19:52:15 +0300

    Commit

--- Meteostation/Meteostation/settings.py
+++ Meteostation/Meteostation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yqn=z4tqv6bo(+n2bj^71@)v3k6k5!=cith8=vs1ay7nqh(kk)'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
