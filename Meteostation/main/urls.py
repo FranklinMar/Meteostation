cdd82abf6511b759f20dc071c69dcc6aa8f268bb
DenysBerezniuk <49430756+FranklinMar@users.noreply.github.com>
2023-03-06 18:36:46 +0200

    Major Update of system: feat(Database): Added new field "Regions" for Object-oriented relational structure and dividing Meteodata "Data" by regions. Old data is transferred to "Lviv" region.

--- Meteostation/main/urls.py
+++ Meteostation/main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('table', views.table, name='table'),
     path('data', views.data, name='data'),
+    # path('data/<slug:name>', views.TableView.as_view(), name='data-table')
+    path('data/<str:name>', views.table, name='data-table')
 ]
